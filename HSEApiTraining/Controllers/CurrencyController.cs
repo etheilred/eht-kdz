3703cb710539bd4ab7f69fbb668057569197b6b2
Arhisan <arhisans@gmail.com>
2020-02-24 22:55:11 +0300

    redundancy removed

--- HSEApiTraining/Controllers/CurrencyController.cs
+++ HSEApiTraining/Controllers/CurrencyController.cs
@@ -2,6 +2,7 @@
 
 namespace HSEApiTraining.Controllers
 {
+    //Тут все методы-хендлеры вам нужно реализовать самим
     [Route("api/[controller]")]
     [ApiController]
     public class CurrencyController : Controller
--- HSEApiTraining/Models/Calculator/CalculatorResponse.cs
+++ HSEApiTraining/Models/Calculator/CalculatorResponse.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace HSEApiTraining.Models.Calculator
+﻿namespace HSEApiTraining.Models.Calculator
 {
     public class CalculatorResponse
     {
