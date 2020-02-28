15ea014e29485ffd909b1fb99c03df4fd9356696
roman <rdandrievskiy@edu.hse.ru>
2020-02-28 23:33:23 +0300

    Currency name validity check implemented

--- HSEApiTraining/Services/CurrencyService.cs
+++ HSEApiTraining/Services/CurrencyService.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace HSEApiTraining
@@ -9,13 +10,12 @@ namespace HSEApiTraining
     public interface ICurrencyService
     {
         IEnumerable<double> GetRates(string currency, DateTime? start, DateTime? end);
-
-        string Error { get; }
     }
 
     public class CurrencyService : ICurrencyService
     {
-        private static readonly HttpClient _client = new HttpClient();
+        private static readonly HttpClient Client = new HttpClient();
+        private static readonly Regex CurrencyRegex = new Regex(@"^[A-Z]{3}$");
 
         public IEnumerable<double> GetRates(string currency, DateTime? start, DateTime? end)
         {
@@ -25,21 +25,24 @@ namespace HSEApiTraining
 
         private async Task<IEnumerable<double>> GetRatesAsync(string currency, DateTime? start, DateTime? end)
         {
+            if (!CurrencyRegex.IsMatch(currency))
+                throw new FormatException($"`{currency}` is not a currency shortcut");
+
             HttpResponseMessage msg = new HttpResponseMessage();
             if (start == null)
             {
-                msg = await _client.GetAsync($"https://api.ratesapi.io/api/latest?base=RUB&symbols={currency}");
+                msg = await Client.GetAsync($"https://api.ratesapi.io/api/latest?base=RUB&symbols={currency}");
             }
             else if (end == null)
             {
-                msg = await _client.GetAsync($"https://api.ratesapi.io/api/{start.Value.Year}-{start.Value.Month}-{start.Value.Day}?base=RUB&symbols={currency}");
+                msg = await Client.GetAsync($"https://api.ratesapi.io/api/{start.Value.Year}-{start.Value.Month}-{start.Value.Day}?base=RUB&symbols={currency}");
             }
             else
             {
                 List<double> res = new List<double>();
-                for (DateTime t = start.Value; t < end.Value; t = t.AddDays(1))
+                for (DateTime t = start.Value; t <= end.Value; t = t.AddDays(1))
                 {
-                    msg = await _client.GetAsync($"https://api.ratesapi.io/api/{t.Year}-{t.Month}-{t.Day}?base=RUB&symbols={currency}");
+                    msg = await Client.GetAsync($"https://api.ratesapi.io/api/{t.Year}-{t.Month}-{t.Day}?base=RUB&symbols={currency}");
                     msg.EnsureSuccessStatusCode();
                     res.Add(double.Parse(JObject.Parse(msg.Content.ReadAsStringAsync().Result)["rates"][currency].ToString()));
                 }
@@ -48,9 +51,7 @@ namespace HSEApiTraining
             msg.EnsureSuccessStatusCode();
             JObject obj = JObject.Parse(await msg.Content.ReadAsStringAsync());
             if (!obj.ContainsKey("rates")) return null;
-            return new List<double> { double.Parse(obj["rates"]["USD"].ToString()) };
+            return new List<double> { double.Parse(obj["rates"][currency].ToString()) };
         }
-
-        public string Error => null;
     }
 }
